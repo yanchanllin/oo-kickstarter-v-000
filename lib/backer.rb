006aec32d93312340ebfcf549292be3b63db56c4
YanC Lin <yonalin1@gmail.com>
2017-11-17 17:57:16 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,12 @@
 class Backer
-  attr_accessor :name
-
+  attr_accessor :name, :project
 
     def initialize(name)
         @name = name
       end
 
+    def back_project 
+      backed_projects = []
+    end 
 
 end
