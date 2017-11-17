c64eb3b64cce9920a5a223bfab9578b1b25b9e8e
YanC Lin <yonalin1@gmail.com>
2017-11-17 18:12:44 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,13 @@
 class Project
   attr_accessor :title
 
-
+   backers = []
     def initialize(title)
         @title = title
       end
-
+   def add_backer(Backer) 
+     backer = Backer.new
+     backers << backer
+   end 
+   
 end 
